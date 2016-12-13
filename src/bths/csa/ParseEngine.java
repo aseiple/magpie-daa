f10c562770ae55ccd33460ad35fd58ea10c6dbc7
August Seiple <aseiple678@gmail.com>
2016-12-13 13:53:07 -0500

    Removed Parsing

--- src/bths/csa/ParseEngine.java
+++ src/bths/csa/ParseEngine.java
@@ -1,46 +1,7 @@
 package bths.csa;
 
-import opennlp.tools.cmdline.parser.ParserTool;
-import opennlp.tools.parser.Parse;
-import opennlp.tools.parser.Parser;
-import opennlp.tools.parser.ParserFactory;
-import opennlp.tools.parser.ParserModel;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
 public class ParseEngine {
-    private ParserModel model;
-    private Parser parser;
-
-    public ParseEngine() throws FileNotFoundException {
-        InputStream modelIn = new FileInputStream("en-parser-chunking.bin");
-        try {
-            model = new ParserModel(modelIn);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            if (modelIn != null) {
-                try {
-                    modelIn.close();
-                }
-                catch (IOException e) {
-                }
-            }
-        }
-
-        parser = ParserFactory.create(model);
-    }
-
-    public String getReply(String in) {
-        return parse(in)[0].toString();
-    }
-
-    private Parse[] parse(String s) {
-        return ParserTool.parseLine(s, parser, 1);
+    public String getReply(String s) {
+        return null;
     }
 }
