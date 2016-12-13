72a43e82721dda94c690b9b0ea3482a2eb0a08f5
August Seiple <aseiple678@gmail.com>
2016-12-12 19:54:44 -0500

    Started Parser Work

--- src/bths/csa/RunMagpie.java
+++ src/bths/csa/RunMagpie.java
@@ -1,16 +1,15 @@
 package bths.csa;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class RunMagpie {
-    public static void main(String[] args) {
-        String response = null;
-        Parser parse = new Parser();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Hello!");
-        while (!response.toLowerCase().equals("bye")) {
+     public static void main(String[] args) throws FileNotFoundException {
+            String response = null;
+            ParseEngine parse = new ParseEngine();
+            System.out.println("Hello!");
+            Scanner sc = new Scanner(System.in);
             response = sc.nextLine();
             System.out.println(parse.getReply(response));
-        }
-    }
+     }
 }
