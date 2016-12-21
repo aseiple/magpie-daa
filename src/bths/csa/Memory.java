fab2c535db943a616f09d5032a50f259f9c41b4d
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2016-12-21 13:41:32 -0500

    Made the basic structure of memory with getting questions and answers

--- src/bths/csa/Memory.java
+++ src/bths/csa/Memory.java
@@ -1,4 +1,26 @@
 package bths.csa;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Memory {
+	
+	private Queue questions = new LinkedList();
+	private Queue answers = new LinkedList();
+	
+	public void getQuestion(String question)
+	{
+		questions.offer(question);
+	}
+	
+	public void getAnswer(String answer)
+	{
+		answers.offer(answer);
+	}
+	
+	public String giveAnswerTo(String question)
+	{
+		String[] questions = new String[questions.length];
+		return question;
+	}
 }
